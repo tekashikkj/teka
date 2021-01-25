6edfe42ffb3c997ce70f189fcd7abdc5cddf4556
tekashikkj <77955071+tekashikkj@users.noreply.github.com>
2021-01-25 03:56:24 -0300

    Add files via upload

---
+++ darkmenu.js
@@ -0,0 +1,38 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Dark:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *DARK*
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
